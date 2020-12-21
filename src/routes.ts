748e0c597b49f135b0c75daa47345d6537865959
FelipeColona <72169708+FelipeColona@users.noreply.github.com>
2020-12-21 19:21:35 -0300

    feat: join room and real-time private chat finalization

--- src/routes.ts
+++ src/routes.ts
@@ -11,24 +11,46 @@ routes.get('/', (req: Request, res: Response) => {
     res.render('index.html')
 })
 
-routes.post('/create', async (req, res) => {
+routes.post('/create', async (req: Request, res: Response) => {
     res.render('create.html')
 
-    io.on('connection', async (socket) => {
+    io.on('connection', async (socket: socketIO.Socket) => {
         console.log(`Socket: ${socket.id}`)
 
-        socket.join(socket.id)
         io.in(socket.id).emit('genLink', socket.id)
 
         // Listen for the chat message
         socket.in(socket.id).on('messageClient', msg => {
-            io.in(socket.id).emit('messageServer', {author: 'Unknow', message: msg})
+            io.in(socket.id).emit('messageServer', {author: 'Felipe', message: msg})
         })
     })
 })
 
-// Just for tests
-routes.get('/list/:room', async (req, res) => {
+routes.get('/join/:room', async (req: Request, res: Response) => {
+    res.render('private.html')
+
+    const {room} = req.params
+
+    io.on('connection', async (socket: socketIO.Socket) => {
+        //@ts-ignore
+        const clientsInRoom: Set = await io.in(room).allSockets()
+        const clientsInRoomArray: string[] = Array.from(clientsInRoom)
+
+        if(clientsInRoomArray.length === 2){
+            console.log('The room is full!')
+        }else{
+            socket.leave(socket.id)
+            socket.join(room)
+            io.sockets.in(room).emit('messageServer', {author: 'Bot', message: `Player ${clientsInRoomArray.indexOf(socket.id) + 1} has entered the chat...`})
+        }
+
+        socket.in(room).on('messageClient', msg => {
+            io.in(room).emit('messageServer', {author: 'Felipe', message: msg})
+        })
+    })
+})
+
+routes.get('/list/:room', async (req: Request, res: Response) => {
 
     const {room} = req.params
 
@@ -38,6 +60,12 @@ routes.get('/list/:room', async (req, res) => {
     console.log(clientsInRoom)
 
     res.send(Array.from(clientsInRoom))
+})
+
+//just for tests
+routes.get('/list', (req: Request, res: Response) => {
+    console.log(io.sockets.adapter.rooms)
+    res.send(io.sockets.adapter.rooms)
 })
 
 export default routes
